b319335580b3bcc74ac0f3bc4500025a752ab4c7
Caragine <ac83779p@pace.edu>
2024-05-13 17:33:56 -0400

    adding comments

--- PythonFileSorter.py
+++ PythonFileSorter.py
@@ -11,16 +11,18 @@ path = user_file
 # Get list of file names
 file_names = os.listdir(path)
 
-# Find every file extension present in folder and create folders for each
+# Find every file extension present in directory and add them to folder_names
 folder_names = []
 for file in file_names:
     if os.path.splitext(file)[1] not in folder_names:
         folder_names.append(os.path.splitext(file)[1])
 
-for loop in range(len(folder_names)):
-    if not os.path.exists(path + folder_names[loop]):
-        os.mkdir(path + folder_names[loop])
+# Create folders with extension names
+for ext in range(len(folder_names)):
+    if not os.path.exists(path + folder_names[ext]):
+        os.mkdir(path + folder_names[ext])
 
+# Loop through files and move them to corresponding folder
 for file in file_names:
     ext = os.path.splitext(file)[1]
     shutil.move(path + file, path + ext + "/" + file)
